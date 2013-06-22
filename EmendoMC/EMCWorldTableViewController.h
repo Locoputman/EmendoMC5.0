1c1782c022f157b88f3164b44b98c4d6b25c4995
Silas <redwing.climb@gmail.com>
2013-06-22 11:38:51 -0600

    open source

---
+++ EmendoMC/EMCWorldTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  EMCWorldTableViewController.h
+//  EmendoMC5.0
+//
+//  Created by Silas Schwarz on 6/7/13.
+//  Copyright (c) 2013 Silas Schwarz. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface EMCWorldTableViewController : UITableViewController
+
+@end
